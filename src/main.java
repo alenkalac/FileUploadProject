d6a2be885628574c0e98b6460fdc4d847e39d890
alenkalac <alenkalac87@gmail.com>
2016-04-22 14:24:01 +0100

    Initial Commit

---
+++ src/main.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author Alen Kalac
+ *
+ */
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+
+}
